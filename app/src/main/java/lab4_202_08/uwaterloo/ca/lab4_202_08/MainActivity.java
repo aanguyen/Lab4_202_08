c2bf8b1f321497bbf3c1a24307de28305439050f
Andrew Nguyen <andrew.nguyen.waterloo@gmail.com>
2017-09-14 18:27:49 -0400

    Got button working

--- app/src/main/java/lab4_202_08/uwaterloo/ca/lab4_202_08/MainActivity.java
+++ app/src/main/java/lab4_202_08/uwaterloo/ca/lab4_202_08/MainActivity.java
@@ -12,13 +12,16 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import java.io.FileWriter;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+
+import ca.uwaterloo.sensortoy.LineGraphView;
 
 public class MainActivity extends AppCompatActivity {
-
-    //LineGraphView graph;
+    final String APP_NAME = "LAB4_202_08";
+    LineGraphView graph;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,19 +36,38 @@ public class MainActivity extends AppCompatActivity {
         TextView title = new TextView(getApplicationContext());
         TextView accelText = new TextView(getApplicationContext());
         Button myButton = new Button (getApplicationContext());
-
-        title.setText("TITLE TEST");
+        graph = new LineGraphView(getApplicationContext(), 100, Arrays.asList("x", "y", "z"));
+        title.setText("lorem ipsum");
         title.setTextSize(40);
 
         myButton.setText("Make CSV");
+
+
+        //Add all views to linear layout to the linear layout
+        l.addView(title);
+        l.addView(accelText);
+        l.addView(myButton);
+        l.addView(graph);
+        graph.setVisibility(View.VISIBLE);
+
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        final Sensor accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //final SensorEventListener accelHandler = new AccelerometerEventListener(accelText, graph);
+        final AccelerometerEventListener accelHandler = new AccelerometerEventListener(accelText, graph);
+        sensorManager.registerListener(accelHandler, accelSensor, sensorManager.SENSOR_DELAY_GAME);
         myButton.setOnClickListener(new View.OnClickListener() {
             public void onClick (View v) {
-                FileWriter fw = null;
                 PrintWriter pw = null;
                 try {
-                    fw = new FileWriter("filename.csv");
-                    pw = new PrintWriter(fw);
-                    //Write the coordinates to the csv here
+                    //CHANGE CSV NAME BELOW to reflect which direction data it is!!
+                    File file = new File(getExternalFilesDir(APP_NAME), "DATA.csv");
+                    pw = new PrintWriter(file);
+                    for (int i = 0; i < 100; i++) {
+                        float[] readingToPrint = accelHandler.getReadingAtIndex(i);
+                        String strToPrint = readingToPrint[0] + "," + readingToPrint[1] + "," + readingToPrint[2];
+                        pw.println(strToPrint);
+                    }
                 } catch (IOException e) {
                     Log.d("File Write Error: ", e.toString());
                 } finally {
@@ -57,34 +79,42 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-        //Add all views to linear layout to the linear layout
-        l.addView(title);
-        l.addView(accelText);
-        l.addView(myButton);
-
-        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-
-        Sensor accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        SensorEventListener accel = new AccelerometerEventListener(accelText);
-        sensorManager.registerListener(accel, accelSensor, sensorManager.SENSOR_DELAY_GAME);
-
     }
-
 }
 class AccelerometerEventListener implements SensorEventListener {
     TextView output;
+    LineGraphView outGraph;
+    public float[][] storedVals = new float[3][100];
     //constructor
-    public AccelerometerEventListener(TextView outputView) {
+    public AccelerometerEventListener(TextView outputView, LineGraphView graph) {
         output = outputView;
+        outGraph = graph;
     }
 
     public void onAccuracyChanged(Sensor s, int i) {}
 
     public void onSensorChanged(SensorEvent se) {
         if (se.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            //put the stuff into the linked list and graph
-            output.setText("CURRENT: X: " + String.valueOf(se.values[0]) + ", Y: " + String.valueOf(se.values[1])+ ", Z: " + String.valueOf(se.values[2]));
+            //Filter point here
+            float[] filteredReading = new float[3];
+
+
+            storeValue(se.values[0], se.values[1], se.values[2]);
+            outGraph.addPoint(se.values);
+            output.setText(String.format("X:%.3f\nY:%.3f \nZ:%.3f", se.values[0], se.values[1], se.values[2]));
         }
+    }
+    private void storeValue(float x, float y, float z) {
+        for (int i = 0; i < 99; i++) {
+            storedVals[0][i] = storedVals[0][i+1];
+            storedVals[1][i] = storedVals[1][i+1];
+            storedVals[2][i] = storedVals[2][i+1];
+        }
+        storedVals[0][99] = x;
+        storedVals[1][99] = y;
+        storedVals[2][99] = z;
+    }
+    public float[] getReadingAtIndex(int i) {
+        return new float[]{storedVals[0][i], storedVals[1][i], storedVals[2][i]};
     }
 }
